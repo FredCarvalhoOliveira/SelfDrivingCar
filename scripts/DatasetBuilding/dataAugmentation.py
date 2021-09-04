Fixed vision feature calculation Added tensorboard metrics for cnn training cycle Added dataset transform for mlp training

scripts/DatasetBuilding/dataAugmentation.py
@@ -6,10 +6,16 @@ import random
 from datasetParser import DatasetParser
 from datasetBuilder import DatasetBuilder
 
+from scripts.imageProcessing import ImageProcessing
+from scripts.utils import loadCalibValues
+
+
 
 class DataAugmentation:
     def __init__(self):
-        pass
+        calibValues = loadCalibValues("../../res/calibration_values_new")
+        self.__imgProcess  = ImageProcessing()
+        self.__imgProcess.setCalibValues(calibValues)
 
     def generateLightVariations(self, img, numVariations):
         height, width = img.shape
@@ -61,6 +67,16 @@ class DataAugmentation:
         mirrored = np.flip(img, 1)  # Mirror image
         cmds[0] *= -1  # Flip steering
         return mirrored, cmds
+
+    def transformImgToFeatures(self, img):
+        # Feature extraction
+        croppedImg = self.__imgProcess.cropFrame(img)
+        binImg     = self.__imgProcess.segmentFrame(croppedImg)
+        roi        = self.__imgProcess.applyRoiMask(binImg)
+        warp_img   = self.__imgProcess.applyBirdsEyePerspective(roi)
+        curv, centerOffset, coefs = self.__imgProcess.extractLaneFeatures(warp_img)
+        features = np.hstack((np.array([curv, centerOffset]), coefs))
+        return features
 
 
 
@@ -120,22 +136,25 @@ if __name__ == "__main__":
     # variations = augment.generateLightVariations(img, 25)
     # variations = augment.generateShadowVariations(img, 25)
 
+    variations = augment.transformImgToFeatures(img)
+    print(variations)
+
     controls = [0.83, 0.61]
 
-    variations = augment.mirrorData(img, [0, 0])
+    # variations = augment.mirrorData(img, [0, 0])
     #
 
-    plt.figure("Data Augmentation", figsize=(15, 7))
-    plt.subplot(1, 2, 1)
-    plt.title("Aceleração = " + str(controls[0]) + "  Direção = " + str(controls[1]), fontsize=20)
-
-    plt.imshow(img, cmap='gray')
-    plt.axis('off')
-
-    plt.subplot(1, 2, 2)
-    plt.title("Aceleração = " + str(controls[0]) + "  Direção = " + str(controls[1] * -1), fontsize=20)
-    plt.imshow(variations[0], cmap='gray')
-    plt.axis('off')
+    # plt.figure("Data Augmentation", figsize=(15, 7))
+    # plt.subplot(1, 2, 1)
+    # plt.title("Aceleração = " + str(controls[0]) + "  Direção = " + str(controls[1]), fontsize=20)
+    #
+    # plt.imshow(img, cmap='gray')
+    # plt.axis('off')
+    #
+    # plt.subplot(1, 2, 2)
+    # plt.title("Aceleração = " + str(controls[0]) + "  Direção = " + str(controls[1] * -1), fontsize=20)
+    # plt.imshow(variations[0], cmap='gray')
+    # plt.axis('off')
 
     # plt.figure("Data Augmentation", figsize=(15, 7))
     # for i in range(25):
@@ -143,5 +162,5 @@ if __name__ == "__main__":
     #     plt.imshow(variations[i], cmap='gray')
     #     plt.axis('off')
 
-    plt.tight_layout()
-    plt.show()
+    # plt.tight_layout()
+    # plt.show()

scripts/DatasetBuilding/manualDriving_Controller.py
@@ -43,9 +43,9 @@ while True:
    db.addDataLine(frame, np.array([leftVertical, rightHorizontal]))
 
 
-   scale = 5
-   frame = imutils.resize(frame, frame.shape[1] * scale, frame.shape[0] * scale)
-   # # Feature extraction
+   # scale = 5
+   # frame = imutils.resize(frame, frame.shape[1] * scale, frame.shape[0] * scale)
+   # Feature extraction
    # croppedImg = imgProcess.cropFrame(frame)
    # binImg     = imgProcess.segmentFrame(croppedImg)
    # roi        = imgProcess.applyRoiMask(binImg)

scripts/modelCNN.py
@@ -5,6 +5,8 @@ import torch.nn.functional as F
 from tqdm import tqdm
 from DatasetBuilding.drivingDataset import DrivingDataset
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
 
 
 
@@ -74,7 +76,7 @@ if __name__ == "__main__":
    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
    # Train params
-   numEpochs = 500
+   numEpochs = 3#500
    batchSize = 100
    learningRate = 0.001
 
@@ -89,6 +91,9 @@ if __name__ == "__main__":
    criterion = nn.MSELoss()
    optimizer = optim.Adam(model.parameters(), lr=learningRate)
 
+   writer = SummaryWriter(f'runs/CNN/cropped')
+
+   step = 0
    # Train
    for epoch in range(numEpochs):
       for batchIdx, (data, targets) in enumerate(dataloader):
@@ -105,5 +110,9 @@ if __name__ == "__main__":
 
          # Optimizer step
          optimizer.step()
+
+         writer.add_scalar('Training Loss', loss, global_step=step)
+         step += 1
+
       print("Epoch #" + str(epoch + 1) + " Loss: " + str(loss))
    torch.save(model.state_dict(), '../res/models/FINAL_CNN_epochs_' + str(epoch + 1))